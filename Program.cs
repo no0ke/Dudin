05dddce23f4120894473603fa269bcdeb254405a
Sergey Dudin <No0ke@yandex.ru>
2022-06-23 22:04:59 +0300

    1 задача!

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1. Напишите программу, которая на вход принимает два
+// числа и проверяет, является ли первое число квадратом
+// второго.
+Console.WriteLine ("Введите число 1!");
+ int a = Convert.ToInt32(Console.ReadLine ());
+ Console.WriteLine ("Введите число 2!");
+ int b = Convert.ToInt32(Console.ReadLine ());
+if (a == b * b) {
+    Console.WriteLine ("Верно!");
+}
+else Console.WriteLine ("Не верно");
+
